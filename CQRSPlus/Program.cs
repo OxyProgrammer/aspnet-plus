1791c2b9e8e7de9863b668a382df52351f1785ac
oxyprogrammer <oxyprogrammer@gmail.com>
2024-03-12 12:01:40 +0000

    Added extesnion methods and right configurations.

--- CQRSPlus/Program.cs
+++ CQRSPlus/Program.cs
@@ -1,14 +1,35 @@
+using CQRSPlus.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
 
 app.UseAuthorization();
 
